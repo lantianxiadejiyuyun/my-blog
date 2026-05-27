f8fe63cf59d6a831d6d7761588b082265927c24e
eugen <2185888430@qq.com>
2026-05-27 22:10:45 +0800

    重新整理项目层级 新增admin-vue 项目

---
+++ vue-view/admin-view/src/router/index.ts
@@ -0,0 +1,10 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [{
+    path:'/',name:'login',component:()=> import('@/views/LoginView.vue')
+  }],
+})
+
+export default router
